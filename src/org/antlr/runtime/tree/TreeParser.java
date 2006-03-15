66aa0bed8538733055de7ffc27309f3aa78e133b
parrt <parrt@antlr.org>
2006-03-14 18:13:09 -0800

    oops; fixed bug.  Added toTemplates functionality to TreeParser
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 2455]

--- src/org/antlr/runtime/tree/TreeParser.java
+++ src/org/antlr/runtime/tree/TreeParser.java
@@ -2,6 +2,9 @@ package org.antlr.runtime.tree;
 
 import org.antlr.runtime.*;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /** A parser for a stream of tree nodes.  "tree grammars" result in a subclass
  *  of this.  All the error reporting and recovery is shared with Parser via
  *  the BaseRecognizer superclass.
@@ -23,7 +26,22 @@ public class TreeParser extends BaseRecognizer {
 		return input;
 	}
 
-	
+
+	/** Convert a List<RuleReturnScope> to List<StringTemplate> by copying
+	 *  out the .st property.  Useful when converting from
+	 *  list labels to template attributes:
+	 *
+	 *    a : ids+=rule -> foo(ids={toTemplates($ids)})
+	 *      ;
+	 */
+	public List toTemplates(List retvals) {
+		if ( retvals==null ) return null;
+		List strings = new ArrayList(retvals.size());
+		for (int i=0; i<retvals.size(); i++) {
+			strings.add(((TreeRuleReturnScope)retvals.get(i)).getTemplate());
+		}
+		return strings;
+	}
 
 	/** We have DOWN/UP nodes in the stream that have no line info; override.
 	 *  plus we want to alter the exception type.
