12b1811858ff582b3d0b9663e4c39328145952d1
Kay Roepke <kroepke@dolphin-services.de>
2007-01-31 14:26:08 -0800

    regenerated hoisted predicate example fixed file references in project
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 3412]

---
+++ lib/ObjC/Framework/examples/hoistedPredicates/TLexer.h
@@ -0,0 +1,32 @@
+// $ANTLR 3.0b6 T.g 2007-01-31 23:22:22
+
+#import <Cocoa/Cocoa.h>
+#import <ANTLR/ANTLR.h>
+
+#pragma mark Cyclic DFA start
+#pragma mark Cyclic DFA end
+
+#pragma mark Rule return scopes start
+#pragma mark Rule return scopes end
+
+#pragma mark Tokens
+#define TLexer_INT	5
+#define TLexer_EOF	-1
+#define TLexer_WS	6
+#define TLexer_Tokens	8
+#define TLexer_T7	7
+#define TLexer_ID	4
+
+@interface TLexer : ANTLRLexer {
+}
+
+
+- (void) mT7;
+- (void) mID;
+- (void) mINT;
+- (void) mWS;
+- (void) mTokens;
+
+
+
+@end
---
+++ lib/ObjC/Framework/examples/hoistedPredicates/TParser.h
@@ -0,0 +1,40 @@
+// $ANTLR 3.0b6 T.g 2007-01-31 23:22:22
+
+#import <Cocoa/Cocoa.h>
+#import <ANTLR/ANTLR.h>
+
+
+#pragma mark Cyclic DFA
+
+#pragma mark Tokens
+#define TParser_INT	5
+#define TParser_WS	6
+#define TParser_EOF	-1
+#define TParser_ID	4
+
+#pragma mark Dynamic Global Scopes
+
+#pragma mark Dynamic Rule Scopes
+
+#pragma mark Rule Return Scopes
+
+
+@interface TParser : ANTLRParser {
+
+					
+
+
+	/** With this true, enum is seen as a keyword.  False, it's an identifier */
+	BOOL enableEnum;
+
+ }
+
+
+- (void) stat;
+- (void) identifier;
+- (void) enumAsKeyword;
+- (void) enumAsID;
+
+
+
+@end
