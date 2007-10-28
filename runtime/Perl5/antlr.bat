4f04658336b8f1dcfcb48898baf3d66e02122de8
Ronald Blaschke <ron@rblasch.org>
2007-10-28 06:02:10 -0800

    [Perl5] Fix antlr script. Move class tests into class subdirectory.
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 4243]

--- runtime/Perl5/antlr.bat
+++ runtime/Perl5/antlr.bat
@@ -4,6 +4,6 @@ IF "%ANTLR_HOME%" == "" SET ANTLR_HOME=%~p0..\..
 
 "%JAVA_HOME%\bin\java" ^
     -Dfile.encoding=windows-1252 ^
-    -classpath "%~p0..\..\build\classes;%~p0..\..\build\rtclasses;%ANTLR_HOME%\lib\antlr-3.0.jar;%ANTLR_HOME%\lib\antlr-2.7.7.jar;%ANTLR_HOME%\lib\stringtemplate-3.0.jar" ^
+    -classpath "%ANTLR_HOME%\build\classes;%ANTLR_HOME%\build\rtclasses;%ANTLR_HOME%\lib\antlr-3.0.jar;%ANTLR_HOME%\lib\antlr-2.7.7.jar;%ANTLR_HOME%\lib\stringtemplate-3.0.jar" ^
     org.antlr.Tool ^
     %*
