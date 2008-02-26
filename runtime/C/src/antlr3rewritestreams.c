797192bebb4470b475779626b0a0b6f88f1f8dc5
Jim Idle <jimi@temporal-wave.com>
2008-02-25 16:32:39 -0800

    Correct accidentally checked in debug edit
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 4674]

--- runtime/C/src/antlr3rewritestreams.c
+++ runtime/C/src/antlr3rewritestreams.c
@@ -31,8 +31,7 @@ static	void				freeRS			(pANTLR3_REWRITE_RULE_ELEMENT_STREAM stream);
 static void
 freeRS	(pANTLR3_REWRITE_RULE_ELEMENT_STREAM stream)
 {
-	if (//stream->freeElements == ANTLR3_TRUE && 
-		stream->elements != NULL)
+	if (stream->freeElements == ANTLR3_TRUE && stream->elements != NULL)
 	{
 		stream->elements->free(stream->elements);
 	}
