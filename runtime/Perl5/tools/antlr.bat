e88de4c59fe5514d1527fb518b280b10e6c5b576
Ronald Blaschke <ron@rblasch.org>
2008-05-17 10:49:29 -0800

    Keep changes local. Avoid MS specific encoding.
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 4926]

--- runtime/Perl5/tools/antlr.bat
+++ runtime/Perl5/tools/antlr.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal
+
 IF "%ANTLR_HOME%" == "" SET ANTLR_HOME=%~d0%~p0..\..\..
 
 "%JAVA_HOME%\bin\java" ^
@@ -7,3 +9,5 @@ IF "%ANTLR_HOME%" == "" SET ANTLR_HOME=%~d0%~p0..\..\..
     -classpath "%ANTLR_HOME%\build\classes;%ANTLR_HOME%\build\rtclasses;%ANTLR_HOME%\lib\antlr-3.0.jar;%ANTLR_HOME%\lib\antlr-2.7.7.jar;%ANTLR_HOME%\lib\stringtemplate-3.0.jar" ^
     org.antlr.Tool ^
     %*
+
+endlocal
