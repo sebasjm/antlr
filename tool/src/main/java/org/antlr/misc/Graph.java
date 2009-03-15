f126735348832464bd97eef35773c2e151fd86bc
parrt <parrt@antlr.org>
2009-03-14 18:10:03 -0800

    add graph sorting
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 5860]

---
+++ tool/src/main/java/org/antlr/misc/Graph.java
@@ -0,0 +1,76 @@
+package org.antlr.misc;
+
+import java.util.*;
+
+public class Graph {
+
+    public static class Node {
+        Object payload;
+        List<Node> edges; // depends on which nodes?
+
+        public Node(Object payload) { this.payload = payload; }
+
+        public void addEdge(Node n) {
+            if ( edges==null ) edges = new ArrayList<Node>();
+            if ( !edges.contains(n) ) edges.add(n);
+        }
+
+        public String toString() { return payload.toString(); }
+    }
+
+    /** Map from node payload to node containing it */
+    protected Map<Object,Node> nodes = new HashMap<Object,Node>();
+
+    public void addEdge(Object a, Object b) {
+        //System.out.println("add edge "+a+" to "+b);
+        Node a_node = getNode(a);
+        Node b_node = getNode(b);
+        a_node.addEdge(b_node);
+    }
+
+    protected Node getNode(Object a) {
+        Node existing = nodes.get(a);
+        if ( existing!=null ) return existing;
+        Node n = new Node(a);
+        nodes.put(a, n);
+        return n;
+    }
+
+    /** DFS-based topological sort.  A valid sort is the reverse of
+     *  the post-order DFA traversal.  Amazingly simple but true.
+     *  For sorting, I'm not following convention here since ANTLR
+     *  needs the opposite.  Here's what I assume for sorting:
+     *
+     *    If there exists an edge u -> v then u depends on v and v
+     *    must happen before u.
+     *
+     *  So if this gives nonreversed postorder traversal, I get the order
+     *  I want.
+     */
+    public List<Node> sort() {
+        Set<Node> visited = new HashSet<Node>();
+        ArrayList<Node> sorted = new ArrayList<Node>();
+        while ( visited.size() < nodes.size() ) {
+            // pick any unvisited node, n
+            Node n = null;
+            for (Iterator it = nodes.values().iterator(); it.hasNext();) {
+                n = (Node)it.next();
+                if ( !visited.contains(n) ) break;
+            }
+            DFS(n, visited, sorted);
+        }
+        return sorted;
+    }
+
+    public void DFS(Node n, Set<Node> visited, ArrayList<Node> sorted) {
+        if ( visited.contains(n) ) return;
+        visited.add(n);
+        if ( n.edges!=null ) {
+            for (Iterator it = n.edges.iterator(); it.hasNext();) {
+                Node target = (Node) it.next();
+                DFS(target, visited, sorted);
+            }
+        }
+        sorted.add(n);
+    }
+}
---
+++ tool/src/test/java/org/antlr/test/TestTopologicalSort.java
@@ -0,0 +1,58 @@
+package org.antlr.test;
+
+import org.junit.Test;
+import org.antlr.misc.Graph;
+
+import java.util.List;
+
+/** Test topo sort in GraphNode. */
+public class TestTopologicalSort extends BaseTest {
+    @Test
+    public void testFairlyLargeGraph() throws Exception {
+        Graph g = new Graph();
+        g.addEdge("C", "F");
+        g.addEdge("C", "G");
+        g.addEdge("C", "A");
+        g.addEdge("C", "B");
+        g.addEdge("A", "D");
+        g.addEdge("A", "E");
+        g.addEdge("B", "E");
+        g.addEdge("D", "E");
+        g.addEdge("D", "F");
+        g.addEdge("F", "H");
+        g.addEdge("E", "F");
+
+        String expecting = "[H, F, E, D, A, G, B, C]";
+        List nodes = g.sort();
+        String result = nodes.toString();
+        assertEquals(expecting, result);
+    }
+
+    @Test
+    public void testSimpleTokenDependence() throws Exception {
+        Graph g = new Graph();
+        g.addEdge("Java.g", "MyJava.tokens"); // Java feeds off manual token file
+        g.addEdge("Java.tokens", "Java.g");        
+        g.addEdge("Def.g", "Java.tokens");    // walkers feed off generated tokens
+        g.addEdge("Ref.g", "Java.tokens");
+
+        String expecting = "[MyJava.tokens, Java.g, Java.tokens, Def.g, Ref.g]";
+        List nodes = g.sort();
+        String result = nodes.toString();
+        assertEquals(expecting, result);
+    }
+
+    @Test
+    public void testParserLexerCombo() throws Exception {
+        Graph g = new Graph();
+        g.addEdge("JavaLexer.tokens", "JavaLexer.g");
+        g.addEdge("JavaParser.g", "JavaLexer.tokens");
+        g.addEdge("Def.g", "JavaLexer.tokens");
+        g.addEdge("Ref.g", "JavaLexer.tokens");
+
+        String expecting = "[JavaLexer.g, JavaLexer.tokens, JavaParser.g, Def.g, Ref.g]";
+        List nodes = g.sort();
+        String result = nodes.toString();
+        assertEquals(expecting, result);
+    }
+}
