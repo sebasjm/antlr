07b8c9e998db450e0f34561bffdf4116b50efb86
Johannes Luber <jaluber@gmx.de>
2010-01-21 07:21:21 -0800

    [C#] - Changed Assembly names again - Removed some warnings
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 6592]

--- runtime/CSharp2/Sources/Antlr3.Runtime.Tests/RewriteRuleXxxxStreamFixture.cs
+++ runtime/CSharp2/Sources/Antlr3.Runtime.Tests/RewriteRuleXxxxStreamFixture.cs
@@ -97,13 +97,13 @@ namespace Antlr.Runtime.Tests {
 			RewriteRuleTokenStream tokenTest =
 				new RewriteRuleTokenStream(CreateTreeAdaptor(),	description);
 
-			Assert.IsFalse(tokenTest.HasNext(), "tokenTest has to give back false here.");
+			Assert.IsFalse(tokenTest.HasNext, "tokenTest has to give back false here.");
 			Assert.AreEqual(description.ToString(), tokenTest.Description,
 				"Description strings should be equal.");
-			Assert.AreEqual(0, tokenTest.Size(), "The number of elements should be zero.");
+			Assert.AreEqual(0, tokenTest.Count, "The number of elements should be zero.");
 			tokenTest.Reset();
 			Assert.IsTrue(true, "Reset() shouldn't make any problems here.");
-			Assert.AreEqual(0, tokenTest.Size(),
+			Assert.AreEqual(0, tokenTest.Count,
 				"The number of elements should be still zero.");
 			tokenTest.NextNode();
 		}
@@ -116,13 +116,13 @@ namespace Antlr.Runtime.Tests {
 			RewriteRuleSubtreeStream subtreeTest =
 				new RewriteRuleSubtreeStream(CreateTreeAdaptor(), description);
 
-			Assert.IsFalse(subtreeTest.HasNext(), "HasNext() has to give back false here.");
+			Assert.IsFalse(subtreeTest.HasNext, "HasNext has to give back false here.");
 			Assert.AreEqual(description.ToString(), subtreeTest.Description,
 				"Description strings should be equal.");
-			Assert.AreEqual(0, subtreeTest.Size(), "The number of elements should be zero.");
+			Assert.AreEqual(0, subtreeTest.Count, "The number of elements should be zero.");
 			subtreeTest.Reset();
 			Assert.IsTrue(true, "Reset() shouldn't make any problems here.");
-			Assert.AreEqual(0, subtreeTest.Size(),
+			Assert.AreEqual(0, subtreeTest.Count,
 				"The number of elements should be still zero.");
 			subtreeTest.NextNode();
 		}
@@ -134,13 +134,13 @@ namespace Antlr.Runtime.Tests {
 			RewriteRuleNodeStream nodeTest =
 				new RewriteRuleNodeStream(CreateTreeAdaptor(), description);
 
-			Assert.IsFalse(nodeTest.HasNext(), "HasNext() has to give back false here.");
+			Assert.IsFalse(nodeTest.HasNext, "HasNext has to give back false here.");
 			Assert.AreEqual(description.ToString(), nodeTest.Description,
 				"Description strings should be equal.");
-			Assert.AreEqual(0, nodeTest.Size(), "The number of elements should be zero.");
+			Assert.AreEqual(0, nodeTest.Count, "The number of elements should be zero.");
 			nodeTest.Reset();
 			Assert.IsTrue(true, "Reset() shouldn't make any problems here.");
-			Assert.AreEqual(0, nodeTest.Size(),
+			Assert.AreEqual(0, nodeTest.Count,
 				"The number of elements should be still zero.");
 			nodeTest.NextNode();
 		}
@@ -195,54 +195,54 @@ namespace Antlr.Runtime.Tests {
 
 			// Test Add()
 			tokenTest.Add(token1);
-			Assert.AreEqual(1, tokenTest.Size(), "tokenTest should have the size 1 (1).");
-			Assert.IsTrue(tokenTest.HasNext(), "HasNext() should be true here (1).");
+			Assert.AreEqual(1, tokenTest.Count, "tokenTest should have the size 1 (1).");
+			Assert.IsTrue(tokenTest.HasNext, "HasNext should be true here (1).");
 
 			// Test NextNode()
 			CommonTree tree = (CommonTree) tokenTest.NextNode();
 			Assert.AreEqual(token1, tree.Token,
 				"The returned token should be equal to the given token (1).");
-			Assert.AreEqual(1, tokenTest.Size(), "tokenTest should have the size 1 (2).");
-			Assert.IsFalse(tokenTest.HasNext(), "HasNext() should be false here (1).");
+			Assert.AreEqual(1, tokenTest.Count, "tokenTest should have the size 1 (2).");
+			Assert.IsFalse(tokenTest.HasNext, "HasNext should be false here (1).");
 			tokenTest.Reset();
-			Assert.AreEqual(1, tokenTest.Size(), "tokenTest should have the size 1 (3).");
-			Assert.IsTrue(tokenTest.HasNext(), "HasNext() should be true here (2).");
+			Assert.AreEqual(1, tokenTest.Count, "tokenTest should have the size 1 (3).");
+			Assert.IsTrue(tokenTest.HasNext, "HasNext should be true here (2).");
 
 			// Test NextToken()
 			IToken returnedToken = tokenTest.NextToken();
 			Assert.AreEqual(token1, returnedToken,
 				"The returned token should be equal to the given token (2).");
-			Assert.AreEqual(1, tokenTest.Size(), "tokenTest should have the size 1 (4).");
-			Assert.IsFalse(tokenTest.HasNext(), "HasNext() should be false here (2).");
+			Assert.AreEqual(1, tokenTest.Count, "tokenTest should have the size 1 (4).");
+			Assert.IsFalse(tokenTest.HasNext, "HasNext should be false here (2).");
 			tokenTest.Reset();
-			Assert.AreEqual(1, tokenTest.Size(), "tokenTest should have the size 1 (5).");
-			Assert.IsTrue(tokenTest.HasNext(), "HasNext() should be true here (3).");
+			Assert.AreEqual(1, tokenTest.Count, "tokenTest should have the size 1 (5).");
+			Assert.IsTrue(tokenTest.HasNext, "HasNext should be true here (3).");
 
 			// Test NextTree()
 			returnedToken = (IToken) tokenTest.NextTree();
 			Assert.AreEqual(token1, returnedToken,
 				"The returned token should be equal to the given token (3).");
-			Assert.AreEqual(1, tokenTest.Size(), "tokenTest should have the size 1 (6).");
-			Assert.IsFalse(tokenTest.HasNext(), "HasNext() should be false here (2).");
+			Assert.AreEqual(1, tokenTest.Count, "tokenTest should have the size 1 (6).");
+			Assert.IsFalse(tokenTest.HasNext, "HasNext should be false here (2).");
 			tokenTest.Reset();
-			Assert.AreEqual(1, tokenTest.Size(), "tokenTest should have the size 1 (7).");
-			Assert.IsTrue(tokenTest.HasNext(), "HasNext() should be true here (3).");
+			Assert.AreEqual(1, tokenTest.Count, "tokenTest should have the size 1 (7).");
+			Assert.IsTrue(tokenTest.HasNext, "HasNext should be true here (3).");
 
 			// Test, what happens with two elements
 			IToken token2 = CreateToken(2, "test token without any real context");
 
 			tokenTest.Add(token2);
-			Assert.AreEqual(2, tokenTest.Size(), "tokenTest should have the size 2 (1).");
-			Assert.IsTrue(tokenTest.HasNext(), "HasNext() should be true here (4).");
+			Assert.AreEqual(2, tokenTest.Count, "tokenTest should have the size 2 (1).");
+			Assert.IsTrue(tokenTest.HasNext, "HasNext should be true here (4).");
 			returnedToken = tokenTest.NextToken();
 			Assert.AreEqual(token1, returnedToken,
 				"The returned token should be equal to the given token (4).");
-			Assert.AreEqual(2, tokenTest.Size(), "tokenTest should have the size 2 (2).");
-			Assert.IsTrue(tokenTest.HasNext(), "HasNext() should be true here (5).");
+			Assert.AreEqual(2, tokenTest.Count, "tokenTest should have the size 2 (2).");
+			Assert.IsTrue(tokenTest.HasNext, "HasNext should be true here (5).");
 			returnedToken = tokenTest.NextToken();
 			Assert.AreEqual(token2, returnedToken,
 				"The returned token should be equal to the given token (5).");
-			Assert.IsFalse(tokenTest.HasNext(), "HasNext() should be false here (3).");
+			Assert.IsFalse(tokenTest.HasNext, "HasNext should be false here (3).");
 
 			// Test exception
 			tokenTest.NextToken();
@@ -261,44 +261,44 @@ namespace Antlr.Runtime.Tests {
 
 			// Test Add()
 			subtreeTest.Add(tree1);
-			Assert.AreEqual(1, subtreeTest.Size(), "subtreeTest should have the size 1 (1).");
-			Assert.IsTrue(subtreeTest.HasNext(), "HasNext() should be true here (1).");
+			Assert.AreEqual(1, subtreeTest.Count, "subtreeTest should have the size 1 (1).");
+			Assert.IsTrue(subtreeTest.HasNext, "HasNext should be true here (1).");
 
 			// Test NextNode()
 			Assert.AreEqual(tree1, (ITree) subtreeTest.NextNode(),
 				"The returned tree should be equal to the given tree (1).");
-			Assert.AreEqual(1, subtreeTest.Size(), "subtreeTest should have the size 1 (2).");
-			Assert.IsFalse(subtreeTest.HasNext(), "HasNext() should be false here (1).");
+			Assert.AreEqual(1, subtreeTest.Count, "subtreeTest should have the size 1 (2).");
+			Assert.IsFalse(subtreeTest.HasNext, "HasNext should be false here (1).");
 			subtreeTest.Reset();
-			Assert.AreEqual(1, subtreeTest.Size(), "subtreeTest should have the size 1 (3).");
-			Assert.IsTrue(subtreeTest.HasNext(), "HasNext() should be true here (2).");
+			Assert.AreEqual(1, subtreeTest.Count, "subtreeTest should have the size 1 (3).");
+			Assert.IsTrue(subtreeTest.HasNext, "HasNext should be true here (2).");
 			
 			// Test NextTree()
 			CommonTree returnedTree = (CommonTree) subtreeTest.NextTree();
 			Assert.AreEqual(token1, returnedTree.Token,
 				"The returned token should be equal to the given token (3).");
-			Assert.AreEqual(1, subtreeTest.Size(), "subtreeTest should have the size 1 (4).");
-			Assert.IsFalse(subtreeTest.HasNext(), "HasNext() should be false here (2).");
+			Assert.AreEqual(1, subtreeTest.Count, "subtreeTest should have the size 1 (4).");
+			Assert.IsFalse(subtreeTest.HasNext, "HasNext should be false here (2).");
 			subtreeTest.Reset();
-			Assert.AreEqual(1, subtreeTest.Size(), "subtreeTest should have the size 1 (5).");
-			Assert.IsTrue(subtreeTest.HasNext(), "HasNext() should be true here (3).");
+			Assert.AreEqual(1, subtreeTest.Count, "subtreeTest should have the size 1 (5).");
+			Assert.IsTrue(subtreeTest.HasNext, "HasNext should be true here (3).");
 			
 			// Test, what happens with two elements
 			IToken token2 = CreateToken(2, "test token without any real context");
 			ITree tree2 = CreateTree(token2);
 
 			subtreeTest.Add(tree2);
-			Assert.AreEqual(2, subtreeTest.Size(), "subtreeTest should have the size 2 (1).");
-			Assert.IsTrue(subtreeTest.HasNext(), "HasNext() should be true here (4).");
+			Assert.AreEqual(2, subtreeTest.Count, "subtreeTest should have the size 2 (1).");
+			Assert.IsTrue(subtreeTest.HasNext, "HasNext should be true here (4).");
 			returnedTree = (CommonTree) subtreeTest.NextTree();
 			Assert.AreEqual(token1, returnedTree.Token,
 				"The returned token should be equal to the given token (4).");
-			Assert.AreEqual(2, subtreeTest.Size(), "subtreeTest should have the size 2 (2).");
-			Assert.IsTrue(subtreeTest.HasNext(), "HasNext() should be true here (5).");
+			Assert.AreEqual(2, subtreeTest.Count, "subtreeTest should have the size 2 (2).");
+			Assert.IsTrue(subtreeTest.HasNext, "HasNext should be true here (5).");
 			returnedTree = (CommonTree) subtreeTest.NextTree();
 			Assert.AreEqual(token2, returnedTree.Token,
 				"The returned token should be equal to the given token (5).");
-			Assert.IsFalse(subtreeTest.HasNext(), "HasNext() should be false here (3).");
+			Assert.IsFalse(subtreeTest.HasNext, "HasNext should be false here (3).");
 
 			// Test exception
 			subtreeTest.NextTree();
@@ -315,45 +315,45 @@ namespace Antlr.Runtime.Tests {
 
 			// Test Add()
 			nodeTest.Add(tree1);
-			Assert.AreEqual(1, nodeTest.Size(), "nodeTest should have the size 1 (1).");
-			Assert.IsTrue(nodeTest.HasNext(), "HasNext() should be true here (1).");
+			Assert.AreEqual(1, nodeTest.Count, "nodeTest should have the size 1 (1).");
+			Assert.IsTrue(nodeTest.HasNext, "HasNext should be true here (1).");
 
 			// Test NextNode()
 			CommonTree returnedTree = (CommonTree) nodeTest.NextNode();
 			Assert.AreEqual(tree1.Type, returnedTree.Type,
 				"The returned tree should be equal to the given tree (1).");
-			Assert.AreEqual(1, nodeTest.Size(), "nodeTest should have the size 1 (2).");
-			Assert.IsFalse(nodeTest.HasNext(), "HasNext() should be false here (1).");
+			Assert.AreEqual(1, nodeTest.Count, "nodeTest should have the size 1 (2).");
+			Assert.IsFalse(nodeTest.HasNext, "HasNext should be false here (1).");
 			nodeTest.Reset();
-			Assert.AreEqual(1, nodeTest.Size(), "nodeTest should have the size 1 (3).");
-			Assert.IsTrue(nodeTest.HasNext(), "HasNext() should be true here (2).");
+			Assert.AreEqual(1, nodeTest.Count, "nodeTest should have the size 1 (3).");
+			Assert.IsTrue(nodeTest.HasNext, "HasNext should be true here (2).");
 
 			// Test NextTree()
 			returnedTree = (CommonTree) nodeTest.NextTree();
 			Assert.AreEqual(token1, returnedTree.Token,
 				"The returned token should be equal to the given token (3).");
-			Assert.AreEqual(1, nodeTest.Size(), "nodeTest should have the size 1 (4).");
-			Assert.IsFalse(nodeTest.HasNext(), "HasNext() should be false here (2).");
+			Assert.AreEqual(1, nodeTest.Count, "nodeTest should have the size 1 (4).");
+			Assert.IsFalse(nodeTest.HasNext, "HasNext should be false here (2).");
 			nodeTest.Reset();
-			Assert.AreEqual(1, nodeTest.Size(), "nodeTest should have the size 1 (5).");
-			Assert.IsTrue(nodeTest.HasNext(), "HasNext() should be true here (3).");
+			Assert.AreEqual(1, nodeTest.Count, "nodeTest should have the size 1 (5).");
+			Assert.IsTrue(nodeTest.HasNext, "HasNext should be true here (3).");
 
 			// Test, what happens with two elements
 			IToken token2 = CreateToken(2, "test token without any real context");
 			ITree tree2 = CreateTree(token2);
 
 			nodeTest.Add(tree2);
-			Assert.AreEqual(2, nodeTest.Size(), "nodeTest should have the size 2 (1).");
-			Assert.IsTrue(nodeTest.HasNext(), "HasNext() should be true here (4).");
+			Assert.AreEqual(2, nodeTest.Count, "nodeTest should have the size 2 (1).");
+			Assert.IsTrue(nodeTest.HasNext, "HasNext should be true here (4).");
 			returnedTree = (CommonTree) nodeTest.NextTree();
 			Assert.AreEqual(token1, returnedTree.Token,
 				"The returned token should be equal to the given token (4).");
-			Assert.AreEqual(2, nodeTest.Size(), "nodeTest should have the size 2 (2).");
-			Assert.IsTrue(nodeTest.HasNext(), "HasNext() should be true here (5).");
+			Assert.AreEqual(2, nodeTest.Count, "nodeTest should have the size 2 (2).");
+			Assert.IsTrue(nodeTest.HasNext, "HasNext should be true here (5).");
 			returnedTree = (CommonTree) nodeTest.NextTree();
 			Assert.AreEqual(token2, returnedTree.Token,
 				"The returned token should be equal to the given token (5).");
-			Assert.IsFalse(nodeTest.HasNext(), "HasNext() should be false here (3).");
+			Assert.IsFalse(nodeTest.HasNext, "HasNext should be false here (3).");
 
 			// Test exception
 			nodeTest.NextTree();
